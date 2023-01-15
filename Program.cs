e6767b7d908d804bca618c3a4c157aced27c52ff
Daniil Gorbunov <dagoroff@gmail.com>
2023-01-15 23:40:09 +0600

    Added solution for task 8

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Welcome to the even numbers from 1 finder!");
+Console.WriteLine("Please, insert your number and we will show all even numbers from 1 to the one, that you inserted:");
+var input = Console.ReadLine();
+if (input == "1")
+{
+    Console.WriteLine("\u2205");
+    return;
+}
+
+var targetNumber = default(int);
+if (!int.TryParse(input, out targetNumber))
+{
+    Console.WriteLine("Sorry, program could not handle inserted value... Bye!");
+    return;
+}
+
+var result = "2";
+for (int i = 4; i <= targetNumber; i += 2)
+{
+    result += ", " + i;
+}
+
+Console.WriteLine(result);
